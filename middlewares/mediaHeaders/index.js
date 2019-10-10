3be07aa65286f08de0f7de02f3256de3ea4fd5c1
Emil Dimitrov <emil@senteca.com>
2019-10-10 22:37:35 +0300

    add cache-control middleware

---
+++ middlewares/mediaHeaders/index.js
@@ -0,0 +1,12 @@
+module.exports = strapi => {
+  return {
+    initialize: function() {
+      strapi.app.use(async (ctx, next) => {
+        await next();
+
+        // Set X-Response-Time header
+        ctx.set("cache-control", "max-age=86001");
+      });
+    }
+  };
+};
